13973ffad246d7e6202611fe20b8cc3e97fc4727
Jean-Lionel <nijeanlionel@gmail.com>
2022-08-19 05:41:39 +0200

    initialize git

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import psycopg2
 import os
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
@@ -10,4 +11,22 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+#SQLALCHEMY_DATABASE_URI= '<Put your local database url>'
+
+
+DB_NAME = "exemple"
+DB_USER = "postgres"
+DB_PASS = "lion"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+try:
+    SQLALCHEMY_DATABASE_URI = psycopg2.connect(database=DB_NAME,
+                                               user=DB_USER,
+                                               password=DB_PASS,
+                                               host=DB_HOST,
+                                               port=DB_PORT)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    print("Database connected successfully")
+except:
+    print("Database not connected successfully")
